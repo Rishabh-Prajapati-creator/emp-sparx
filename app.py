3bd8fa99b0866b34d279d9365ffa08d95a35b87f
Rishabh-Prajapati-creator <rishabhprajapati1470@gmail.com>
2026-08-20 12:42:54 +0530

    initial commit

---
+++ app.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+st.title('CAMPUSx')
+
+col1, col2 = st.columns(2)
+
+with col1:
+    st.image('mobile_logo1.png')
+with col2:
+    st.write("""lorem50sajd;ffffffffffffffffffff;uqrrrrrrrrrrrrrrrrrrrrrnafbbbbbbbbbbbbbbdhhhhhhhhhhhhhhhhhhueeeeeeeeeeeeeeeeyralf""")
+
+
